ae413843a0dc685f2a0f8473b1630f7a921f6a59
Mirvaleh123 <valeh.ibrahimov777@gmail.com>
2024-03-28 00:59:18 +0400

    configurations

---
+++ EF_Core_Configurations/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+class AppLicationDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=KOMPUTER;Database=EDepartman;Trusted_Connection=True;TrustServerCertificate=true");
+    }
+}
---
+++ EF_Core_Configurations1/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+ApplicatonDbContext context = new ApplicatonDbContext();
+
+
+#region Configuration | Data Annotations , Fluent API
+
+#region Table-ToTable
+#endregion
+
+#endregion
+
+class ApplicatonDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=MIRVALEH;Database=EDepartman;Trusted_Connection=True;TrustServerCertificate=true");
+    }
+}
