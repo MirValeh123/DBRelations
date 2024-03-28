8a870a2e8aaf85ef18dd1fe908d3d8c20fd61dba
Mirvaleh123 <valeh.ibrahimov777@gmail.com>
2024-03-29 01:51:08 +0400

    test

---
+++ EF_Core_Configurations1/Migrations/20240328214720_mig_1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF_Core_Configurations1.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Schools",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schools", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Peoples",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TestSchoolId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    SchoolId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Peoples", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Peoples_Schools_SchoolId",
+                        column: x => x.SchoolId,
+                        principalTable: "Schools",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Peoples_SchoolId",
+                table: "Peoples",
+                column: "SchoolId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Peoples");
+
+            migrationBuilder.DropTable(
+                name: "Schools");
+        }
+    }
+}
--- EF_Core_Configurations1/Program.cs
+++ EF_Core_Configurations1/Program.cs
@@ -1,19 +1,51 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 ApplicatonDbContext context = new ApplicatonDbContext();
 
 
 #region Configuration | Data Annotations , Fluent API
 
-#region Table-ToTable
+//[Table("Kisiler")]
+class Person
+{
+    public int Id { get; set; }
+
+    //[ForeignKey(nameof(School))]
+    public int TestSchoolId { get; set; }
+    //[Column("Adi", TypeName = "Metin",Order =3)]
+    public string Name { get; set; } = string.Empty;
+    public string FirstName { get; set; }
+    = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public int Age { get; set; }
+
+}
+
+class School
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public ICollection<Person> Persons { get; set; }
+
+}
 #endregion
 
-#endregion
+
 
 class ApplicatonDbContext : DbContext
 {
+    public DbSet<Person> Peoples { get; set; }
+    public DbSet<School> Schools { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer("Server=MIRVALEH;Database=EDepartman;Trusted_Connection=True;TrustServerCertificate=true");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //modelBuilder.Entity<Person>().ToTable("Kisiler");
+        //modelBuilder.Entity<Person>().Property(p => p.Name).HasColumnName("Adi").HasColumnType("Metin").HasColumnOrder(3);
+        modelBuilder.Entity<School>().HasOne(p => p.Persons).WithMany(x=>x.)
     }
 }
